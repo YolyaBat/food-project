82bc47dd8c1053851ea45d58c1f066d07ad19fbe
Yuliia Batrakova <yuliya.batrakova@gmail.com>
2019-06-20 18:49:48 +0300

    Updates

---
+++ precache-manifest.496c1e681b9a35d31db03fd4605d408c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463c1f7950b9f181e2268902006e629a",
+    "url": "/food-project/index.html"
+  },
+  {
+    "revision": "5e28ceb922b70b50efaf",
+    "url": "/food-project/static/css/main.c1558107.chunk.css"
+  },
+  {
+    "revision": "9387d69826631dbe868d",
+    "url": "/food-project/static/js/2.98e9ec0c.chunk.js"
+  },
+  {
+    "revision": "5e28ceb922b70b50efaf",
+    "url": "/food-project/static/js/main.fde79788.chunk.js"
+  },
+  {
+    "revision": "c3999605755585583bb4",
+    "url": "/food-project/static/js/runtime~main.5fda05b1.js"
+  },
+  {
+    "revision": "09ee0ea51b18c04c1d706f0fb3487f15",
+    "url": "/food-project/static/media/cherry.09ee0ea5.png"
+  },
+  {
+    "revision": "b163f5a49d0c958d3933d3d8468bb896",
+    "url": "/food-project/static/media/copyright.b163f5a4.png"
+  },
+  {
+    "revision": "d0727eafe9cd08cd09ebbfa76c48c5d2",
+    "url": "/food-project/static/media/grapes.d0727eaf.png"
+  },
+  {
+    "revision": "d3def9fc87a74a73dea357c783fbe583",
+    "url": "/food-project/static/media/homepage.d3def9fc.jpg"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/food-project/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "937af0f8d8d350f54a63c11ee7641d7c",
+    "url": "/food-project/static/media/logo.937af0f8.png"
+  },
+  {
+    "revision": "f8993d0ff93ca5c185adb4fe4aaa9a51",
+    "url": "/food-project/static/media/lotus-position.f8993d0f.png"
+  },
+  {
+    "revision": "93895e051b3ef6ed77a0969a80b54cd4",
+    "url": "/food-project/static/media/menu-button.93895e05.png"
+  },
+  {
+    "revision": "f317770ffd13d37bdabcdb94cbc5a06a",
+    "url": "/food-project/static/media/myphoto.f317770f.jpg"
+  },
+  {
+    "revision": "1e1d3efbf31dca953909b8777596cef2",
+    "url": "/food-project/static/media/orange.1e1d3efb.png"
+  },
+  {
+    "revision": "4c2e6b35a84ea07579754b0cd82abefc",
+    "url": "/food-project/static/media/scroll.4c2e6b35.png"
+  },
+  {
+    "revision": "d51940db28b52bc999fbeb72825e495c",
+    "url": "/food-project/static/media/search.d51940db.png"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/food-project/static/media/twitter.0251d8ee.png"
+  },
+  {
+    "revision": "237df5eb4ff2e0fcfcdf71a4b54e3e63",
+    "url": "/food-project/static/media/watermelon.237df5eb.png"
+  }
+]);
